6c38e4d24376fa9893d741dc0738f754a21f0d7f
Chaow Porkaew <pchaowmobile@gmail.com>
2015-12-18 08:59:56 +0700

    add user firstname and lastname to admin's navbar

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -14,7 +14,30 @@
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Dashboard</a></li>
                 <li><a href="#">Settings</a></li>
-                <li><a href="#">Profile</a></li>
+                <li>
+                    <div class="btn-group navbar-btn">
+                        <button type="button" class="btn">
+                            @if(Auth::user())
+                                {{Auth::user()->firstname}} {{Auth::user()->lastname}}
+                            @else
+                                Unknown User
+                            @endif
+
+                        </button>
+                        <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">
+                            <span class="caret"></span>
+                            <span class="sr-only">Toggle Dropdown</span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </div>
+                </li>
                 <li><a href="#">Help</a></li>
             </ul>
             <form class="navbar-form navbar-right">
