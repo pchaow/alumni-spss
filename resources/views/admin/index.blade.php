513cf19edb456822ae5a028009d441086818e19f
Chaow Porkaew <pchaowmobile@gmail.com>
2015-12-22 20:47:34 +0700

    update color of layout

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,30 +10,7 @@
     </div>
     <!-- /.row -->
     <div class="row">
-        <div class="col-lg-3 col-md-6">
-            <a href="#">
-                <div class="panel panel-red">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-cloud-upload fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <h3>นำเข้า</h3>
-                                <!--div>New Comments!</div-->
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class="panel-footer">
-                        <span class="pull-left">นำเข้าข้อมูลศิษย์เก่า</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-
-                </div>
-            </a>
-        </div>
         <div class="col-lg-3 col-md-6">
             <a href="/admin/search">
                 <div class="panel panel-green">
@@ -49,7 +26,7 @@
                     </div>
                     <div class="panel-footer">
                         <span class="pull-left">ค้นหาข้อมูลศิษย์เก่า</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right" style="color: #5cb85c;"></i></span>
                         <div class="clearfix"></div>
                     </div>
 
@@ -71,7 +48,31 @@
                     </div>
                     <div class="panel-footer">
                         <span class="pull-left">รายงานสถิติต่างๆ</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right" style="color: #f0ad4e;"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <a href="#">
+                <div class="panel panel-red">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-cloud-upload fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <h3>นำเข้า</h3>
+                                <!--div>New Comments!</div-->
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="panel-footer">
+                        <span class="pull-left">นำเข้าข้อมูลศิษย์เก่า</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right" style="color: #d9534f;"></i></span>
                         <div class="clearfix"></div>
                     </div>
 
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -83,93 +83,22 @@
                         </li>
 
                         <li>
-                            <a href="/admin/search"><i class="fa fa-search fa-fw"></i> Search</a>
+                            <a href="/admin/search"><i class="fa fa-search fa-fw"></i> ค้นหาศิษย์เก่า</a>
                         </li>
                         <li>
-                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> รายงานสถิติ<span class="fa arrow"></span></a>
                             <ul class="nav nav-second-level">
                                 <li>
-                                    <a href="flot.html">Flot Charts</a>
+                                    <a href="#">รายงาน 1</a>
                                 </li>
                                 <li>
-                                    <a href="morris.html">Morris.js Charts</a>
+                                    <a href="#">รายงาน 2</a>
                                 </li>
                             </ul>
                             <!-- /.nav-second-level -->
                         </li>
                         <li>
-                            <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
-                        </li>
-                        <li>
-                            <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="panels-wells.html">Panels and Wells</a>
-                                </li>
-                                <li>
-                                    <a href="buttons.html">Buttons</a>
-                                </li>
-                                <li>
-                                    <a href="notifications.html">Notifications</a>
-                                </li>
-                                <li>
-                                    <a href="typography.html">Typography</a>
-                                </li>
-                                <li>
-                                    <a href="icons.html"> Icons</a>
-                                </li>
-                                <li>
-                                    <a href="grid.html">Grid</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span
-                                        class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Third Level <span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                    </ul>
-                                    <!-- /.nav-third-level -->
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span
-                                        class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="blank.html">Blank Page</a>
-                                </li>
-                                <li>
-                                    <a href="login.html">Login Page</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
+                            <a href="tables.html"><i class="fa fa-cloud-upload fa-fw"></i> นำเข้า</a>
                         </li>
                     </ul>
                 </div>
