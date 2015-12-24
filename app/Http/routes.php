520210dc68b65fa0b1801d8ae87114a345620105
Chaow Porkaew <pchaowmobile@gmail.com>
2015-12-24 08:44:53 +0700

    update route to route::group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,62 +17,70 @@ Route::get('/', function () {
     return view('home.signin');
 });
 
+Route::group(['prefix' => 'admin'], function () {
 
-Route::get('/admin/auth/signin', function () {
-    return view('admin.auth.signin');
-});
+    Route::get('/auth/signin', function () {
+        return view('admin.auth.signin');
+    });
 
-Route::post('/admin/auth/signin', function () {
-    $login = \Input::get('login');
+    Route::post('/auth/signin', function () {
+        $login = \Input::get('login');
 
-    //$login['password'] = \Hash::make($login['password']);
+        //$login['password'] = \Hash::make($login['password']);
 
-    $user = User::where('username', $login['username'])->first();
+        $user = User::where('username', $login['username'])->first();
 
-    if ($user != null) {
-        $usertype = $user->usertype()->first();
-        if ($usertype->name == "ADMIN") {
-            if (\Hash::check($login['password'], $user->password)) {
-                \Auth::login($user);
-                return redirect('/admin/index');
+        if ($user != null) {
+            $usertype = $user->usertype()->first();
+            if ($usertype->name == "ADMIN") {
+                if (\Hash::check($login['password'], $user->password)) {
+                    \Auth::login($user);
+                    return redirect('/admin/index');
+                }
             }
         }
-    }
-    return redirect('/');
+        return redirect('/');
+
+    });
+
+    Route::get('/auth/logout', function () {
+        \Auth::logout();
+        return redirect('/');
+    });
+
+    Route::get('/', function () {
+        return redirect('/admin/index');
+    });
+
+    Route::get('/index', function () {
+        return view('admin.index');
+    });
+
+    Route::get('/search', function () {
+        return view('admin.search');
+    });
+
+    Route::get('/insert', function () {
+        return view('admin.data.insert');
+    });
 
 });
 
-Route::get('/admin/auth/logout',function(){
-    \Auth::logout();
-    return redirect('/');
-});
 
-Route::get('/admin', function () {
-    return redirect('/admin/index');
-});
-Route::get('/admin/index', function () {
-    return view('admin.index');
-});
+Route::group(['prefix'=>'user'],function(){
+    ///// User //////
 
-Route::get('/admin/search', function () {
-    return view('admin.search');
-});
+    Route::get('/', function () {
+        return redirect('/user/index');
+    });
+    Route::get('/index', function () {
+        return view('user.index');
+    });
 
-Route::get('/admin/insert', function () {
-    return view('admin.data.insert');
+    Route::get('/edit', function () {
+        return view('user.profile.edit');
+    });
 });
 
 
 
-///// User //////
-
-Route::get('/user', function () {
-    return redirect('/user/index');
-});
-Route::get('/user/index', function () {
-    return view('user.index');
-});
-
-Route::get('/user/edit', function () {
-    return view('user.profile.edit');
-});
