f9074af42c66853d6aff8efc6d46a408b4c4c5f2
Chaow Porkaew <pchaowmobile@gmail.com>
2015-12-25 20:54:19 +0700

    add migration and seder

--- database/migrations/2015_12_18_005020_create_users_table.php
+++ database/migrations/2015_12_18_005020_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
         });
 
 
+
         Schema::create('users',function(Blueprint $t){
             $t->increments('id');
             $t->string('username')->unique();
