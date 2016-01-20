bc87205a9e3c3ed92a6cfa3236db40c9572082d2
wanasat <s.birdtoday@hotmail.com>
2016-01-20 17:35:16 +0700

    fix

---
+++ app/Models/Workplace.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Workplace extends Model
+{
+    protected $table = 'workplace';
+
+
+    public function alumni()
+    {
+        return $this->belongsTo('app\Models\Alumni');
+    }
+}
